2fb48576be89fccf36899153772375e70b1d2df6
VikhilP <vikhilp98@gmail.com>
2021-07-05 16:40:52 +0100

    Terraform (80%)

--- terraform.tf
+++ terraform.tf
@@ -10,8 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
-  access_key = "AKIAVAR2U44IOTYVB22C"
-  secret_key = "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"
+
 }
 
 resource "aws_vpc" "spring_app" {
@@ -23,6 +22,40 @@ resource "aws_subnet" "subnetA"{
     cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_ec2"
+resource "aws_subnet" "privatesubnetA"{
+    vpc_id = aws_vpc.spring_app.vpc_id
+    cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_instance" "nginx" {
+    ami           = "ami-005e54dee72cc1d00"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.subnetA.subnet_id
+}
+
+resource "aws_instance" "front-end" {
+    ami           = "ami-005e54dee72cc1d00"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.subnetA.subnet_id
+}
+
+resource "aws_instance" "back-end" {
+    ami           = "ami-005e54dee72cc1d00"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.privatesubnetA.subnet_id
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mysql-petclinic"
+  username             = "petclinic"
+  password             = "petclinic"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
 
 
