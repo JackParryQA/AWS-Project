7f7010820ec755fb45a5a49cce7d605b6acd6c68
VikhilP <vikhilp98@gmail.com>
2021-07-07 16:27:00 +0100

    given full path of docker compose for debugging

--- jenkins/kubescripts.sh
+++ jenkins/kubescripts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo docker-compose build
-sudo docker-compose push
-
+sudo /usr/local/bin/docker-compose build
+sudo /usr/local/bin/docker-compose push
+### TO DO ###
+# Add docker-compose to path
 
 kubectl apply -f kubernetes/rest.yaml
 kubectl apply -f kubernetes/angular.yaml
