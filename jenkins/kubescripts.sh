8583635897e9515e810f51e319ccb721f94f1503
VikhilP <vikhilp98@gmail.com>
2021-07-08 15:37:25 +0100

    added update to kubeconfig

--- jenkins/kubescripts.sh
+++ jenkins/kubescripts.sh
@@ -3,7 +3,7 @@ sudo docker-compose build
 sudo docker-compose push
 ### TO DO ###
 # Add docker-compose to path
-
+aws eks update-kubeconfig --name qa_cluster
 kubectl apply -f kubernetes/rest.yaml
 kubectl apply -f kubernetes/angular.yaml
 kubectl apply -f kubernetes/nginx.yaml
